953a6b222e102e6ffc705eb77f682f9d5919ca3e
jeffkarr <jkarr001@gmail.com>
2024-08-07 12:31:12 -0500

    finished LandingPage component

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,38 @@
-import { Col, Row, Button } from "reactstrap";
+import { useState } from 'react';
+import { Container, Col, Row, Button } from 'reactstrap';
+
 import "./LandingPage.css";
 
 const LandingPage = () => {
+  const [ btnSize, setBtnSize ] = useState('lg');
+  
+  window.addEventListener("resize", function(){
+    adjustBtnSize();
+  });
+
+  function adjustBtnSize() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 900) {
+      setBtnSize('lg');
+    }
+    if (screenWidth > 600 && screenWidth < 900) {
+      setBtnSize(' ');
+    }
+    if (screenWidth > 0 && screenWidth < 600) {
+      setBtnSize('sm');
+    }
+  };
+
   return (
-    <div className="container-fluid landingPic">
+    <Container fluid className="landingPic">
       <Row className="landing-btn-row">
-        <Col size="2">
-          <Button size="lg" color="danger" id="landing-btn" href="/home/">
+        <Col xs={{offset: 7, size: 3}} sm={{offset: 8, size: 2}}>
+          <Button size={btnSize} color="danger" href="/home/" >
             Browse Products
           </Button>
         </Col>
       </Row>
-    </div>
+    </Container>
   )
 }
 
