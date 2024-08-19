2b7c33914cf37dc4fba84aad3acd16e7a21c98e5
jeffkarr <jkarr001@gmail.com>
2024-08-19 17:22:09 -0500

    adjusted layout of Coffee Page

--- frontend/src/components/Wrapper/Wrapper.jsx
+++ frontend/src/components/Wrapper/Wrapper.jsx
@@ -1,7 +1,10 @@
+import { Container } from "reactstrap";
 import "./Wrapper.css";
 
 const Wrapper = props => (
-  <div className="wrapper">{props.children}</div>
+  <div className="wrapper-container"> 
+    <div className="wrapper">{props.children}</div>
+  </div>
 );
 
 export default Wrapper
