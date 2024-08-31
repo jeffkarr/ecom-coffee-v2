a8d6a0173dc923f5391d773c3ce7c9fe589897fa
jeffkarr <jkarr001@gmail.com>
2024-08-31 14:51:23 -0500

    added try/catch to routes. Corrected syntax in init.sql.

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -9,44 +9,56 @@ await pgClient.connect();
 
 const router = express.Router();
 
-router.get('/coffeeItems', async (req, res) => {
+router.get('/getCoffeeItems', async (req, res) => {
+  try {
+    const result = await pgClient.query('select * from coffee_items');
 
-  const result = await pgClient.query('select * from coffee_items');
-
-  console.log('----- results from call to db ----');
-  console.log(result.rows);
-  
-  res.send(result);
+    console.log('----- results from call to db ----');
+    console.log(result.rows);
+    
+    res.send(result);
+  } catch(err) {
+    res.send(`Error encountered on /getCoffeeItems route. - ${err}`);  
+  }
 });
 
 router.get('/getDealItems', async (req, res) => {
+  try {
+    const result = await pgClient.query('select * from deal_items');
 
-  const result = await pgClient.query('select * from deal_items');
-
-  console.log('----- results from call to db ----');
-  console.log(result.rows);
-  
-  res.send(result);
+    console.log('----- results from call to db ----');
+    console.log(result.rows);
+    
+    res.send(result);
+  } catch(err) {
+    res.send(`Error encountered on /getDealItems route. - ${err}`);  
+  }
 });
 
 router.get('/getTeaItems', async (req, res) => {
+  try {
+    const result = await pgClient.query('select * from tea_items');
 
-  const result = await pgClient.query('select * from tea_items');
-
-  console.log('----- results from call to db ----');
-  console.log(result.rows);
-  
-  res.send(result);
+    console.log('----- results from call to db ----');
+    console.log(result.rows);
+    
+    res.send(result);
+  } catch(err) {
+    res.send(`Error encountered on /getTeaItems route. - ${err}`);  
+  }
 });
 
 router.get('/getAccessoryItems', async (req, res) => {
+  try {
+    const result = await pgClient.query('select * from accessory_items');
 
-  const result = await pgClient.query('select * from accessory_items');
-
-  console.log('----- results from call to db ----');
-  console.log(result.rows);
-  
-  res.send(result);
+    console.log('----- results from call to db ----');
+    console.log(result.rows);
+    
+    res.send(result);
+  } catch(err) {
+    res.send(`Error encountered on /getAccessoryItems route. - ${err}`);  
+  }
 });
 
 export default router;
